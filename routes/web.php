8c76e6dd21b116a102ec8817baed8f455d346e28
Takiza <takiza.games@gmail.com>
2019-11-19 01:52:30 +0000

    Add tasks

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,37 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = \App\Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks'=>$tasks
+        ]);
+});
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name'=>'required|max:255'
+        ]);
+        
+    if ($validator->fails()){
+        return redirect('/')
+        ->withInput()
+        ->withErrors($validator);
+    }
+    
+    $task = new \App\Task;
+    $task->name = $request->name;
+    $task->save();
+    
+    return redirect('/');
+});
+
+Route::delete('/task/{task}', function (\App\Task $task) {
+    $task->delete();
+    
+    return redirect('/');
 });
 
 Auth::routes();
