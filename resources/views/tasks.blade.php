3cb5b9b424fbff34385073e3132660f4f1898699
Takiza <takiza.games@gmail.com>
2019-11-19 20:02:14 +0000

    v

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 
+<div class="container">
 <div class="card-body">
     @include('errors')
     <form action="{{ url('task') }}" method="POST" class="form-horizontal">
@@ -26,39 +27,27 @@
 </div>
 
 @if(count($tasks) > 0)
+
 <div class="row">
-  
 @foreach($tasks as $task)
-    <div class="col-sm-3">
-        <div class="card">
-            <div class="card-heading">
-                <table class="table table-striped task-table">
-                    <thead>
-                        <th>Task</th>
-                        <th>&nbsp;</th>
-                    </thead>
-                    
-                    <tbody>
-                        <tr>
-                        <td class="table-text">
-                            <div>{{ $task->name }}</div>
-                        </td>
-                        <td>
-                            <form action="{{ url('task/'.$task->id) }}" method="POST">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
+    <div class="col-sm">
+        <div class="card  my-3">
+            <div class="card-body">
+                <h5 class="card-title">Task</h5>
+                <p class="card-text">{{ $task->name }}</p>
+                <form action="{{ url('task/'.$task->id) }}" method="POST">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
                                 
-                                <button class="btn btn-danger">
-                                    Delete
-                                </button>
-                            </form>
-                        </td></tr>
-                    </tbody>
-                </table>
+                    <button class="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
             </div>
         </div>
     </div>
     @endforeach
+    </div>
 </div>
 @endif
 @endsection
